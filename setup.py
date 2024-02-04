494cc07836bd2ebd303a3ce3f4852733d802c4a6
Dama Venkata Vamsi <venkatavamsi560@gmail.com>
2024-02-04 15:24:55 +0530

    First commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+## edit below variables as per your requirements -
+REPO_NAME = "Movie-Recommender-System-Using-Machine-Learning"
+AUTHOR_USER_NAME = "vamsi"
+SRC_REPO = "src"
+LIST_OF_REQUIREMENTS = ['streamlit']
+
+
+setup(
+    name=SRC_REPO,
+    version="0.0.1",
+    author=AUTHOR_USER_NAME,
+    description="A small package for Movie Recommender System",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/venkatavamsidama",
+    packages=[SRC_REPO],
+    license="MIT",
+    python_requires=">=3.9",
+    install_requires=LIST_OF_REQUIREMENTS
+)
